e9506dd8264aaad04c490035f8060d3a087882d4
Abdul qadeer <102538683+qadeer77@users.noreply.github.com>
2022-07-27 23:16:59 +0500

    update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-function offbulb(){
+function offbulb() {
     var img = document.getElementById("img1");
     img.src = "./images/off-images.png";
 }
-function onbulb(){
+function onbulb() {
     var img = document.getElementById("img1");
     img.src = "https://www.freeiconspng.com/thumbs/lightbulb-png/light-bulb-png-bulb-png1247-12.png";
 }

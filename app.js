3afa1408098615c0b1af6cdfb5074a164f880d4c
Abdul qadeer <102538683+qadeer77@users.noreply.github.com>
2022-07-27 23:12:43 +0500

    k-electric-bulb-complete

---
+++ app.js
@@ -0,0 +1,8 @@
+function offbulb(){
+    var img = document.getElementById("img1");
+    img.src = "./images/off-images.png";
+}
+function onbulb(){
+    var img = document.getElementById("img1");
+    img.src = "https://www.freeiconspng.com/thumbs/lightbulb-png/light-bulb-png-bulb-png1247-12.png";
+}
